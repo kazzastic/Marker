0b37b4cc496084fcabb526ded4f0b5d81aa9cf36
kazzastic <rkazim79@gmail.com>
2019-06-09 17:13:20 +0500

    Sign-in and Sign-up pages added

--- js/map.js
+++ js/map.js
@@ -1,12 +1,18 @@
 function get_loc(){
-	axios.post('https://www.googleapis.com/geolocation/v1/geolocate?',{
-		key: 'AIzaSyA7EsmyKLBgtl13SRV5Qn56cowFm2jQhII'
-	})
+	axios.post('https://www.googleapis.com/geolocation/v1/geolocate?key=#AIzaSyA7EsmyKLBgtl13SRV5Qn56cowFm2jQhII')
 	.then(function(response){
 		console.log(response);
+		var lat, long;
+
+		lat = response.data.location.lat;
+		long = response.data.location.lng;
+		console.log(lat, long)
+
 	})
 	.catch(function(error){
 		console.log(error);
-	})
+	});
 }
 get_loc();
+
+navigator.geolocation.getCurrentPosition(loc => console.log(loc));
