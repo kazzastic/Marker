93222e06901e9e8e3c2df521b1de7b44f63324fb
kazzastic <rkazim79@gmail.com>
2019-06-20 17:09:01 +0500

    firebase/localstorage

---
+++ js/database.js
@@ -0,0 +1,18 @@
+var local;
+var show;
+window.localStorage.setItem('name', 'Shameer Server');
+show = window.localStorage.getItem('name');
+console.log(show);
+
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});test.firestore.js
